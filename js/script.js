a1dcb5d32e6169d4fed7bbe9a5fa52de6c654064
Steven Saunders <stevensaunders@Stevens-MacBook-Pro.local>
2019-07-18 14:13:34 -0600

    added JS functionality for mobile menu. Started CSS styling

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
+//MID-PAGE MENU - MULTIPLE ITEMS OPEN AT A TIME
 const accordion = document.querySelectorAll('.slide-menu .slide-btn');
-//MULTIPLT MENU ITEMS OPEN AT A TIME
 for(let i = 0; i < accordion.length; i++) {
   accordion[i].addEventListener('click', function() {
-
     let child = accordion[i].nextElementSibling;
-
     if(!this.classList.contains('active')){
       this.classList.add('active');
       child.style.maxHeight = child.scrollHeight + 'px';
@@ -15,36 +13,17 @@ for(let i = 0; i < accordion.length; i++) {
   });
 };
 
-
-
+//MOBILE MENU OVERLAY
 const navMenu = document.getElementById('main-menu-toggle');
-
 navMenu.addEventListener('click', function(){
   const menu = document.getElementById('main-nav');
-  menu.classList.toggle('hidden');
+  menu.classList.toggle('open');
 })
-
-
-
-
-
-
-
-// //SINGLE MENU ITEM OPEN AT AT TIME
-// for(let i = 0; i < accordion.length; i++) {
-//   accordion[i].addEventListener('click', function() {
-//     for(var i = 0; i < accordion.length; i++) {
-
-//     let child = accordion[i].nextElementSibling;
-
-//     if(!this.classList.contains('active') && this === accordion[i]){
-//       this.classList.add('active');
-//       child.style.maxHeight = child.scrollHeight + 'px';
-//     } else {
-//       this.classList.remove('active');
-//       child.style.maxHeight = '0px'
-//     }
-//   };
-//   });
-// };
-
+const iconSwap = document.getElementById('menu-icon');
+iconSwap.addEventListener('click', function (){
+  if(this.classList.contains('fa-bars')){
+    this.classList.replace('fa-bars', 'fa-times');
+  } else {
+    this.classList.replace('fa-times', 'fa-bars');
+  };
+});
